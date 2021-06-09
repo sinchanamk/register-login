5d787aea82316c80192810fed3ad4ad0b9485651
sinchana M K <mksinchanasonu@gmail.com>
2021-06-09 19:09:15 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+
 // ignore: slash_for_doc_comments
 /** 
 *@param - username,email,mobile,password,confirmPassword
@@ -10,29 +12,32 @@ import 'package:flutter/material.dart';
 */
 void main() {
   runApp(MaterialApp(
-    home: regWidget(),
-    theme:ThemeData(brightness: Brightness.dark),
+    home: RegWidget(),
+   
   ));
 }
 
-class regWidget extends StatefulWidget
+class RegWidget extends StatefulWidget
 {
   static const String id='register_screen';
   @override
- _regFormWidgetState createState()=>_regFormWidgetState();
+ _RegFormWidgetState createState()=>_RegFormWidgetState();
  
 }
 // ignore: camel_case_types
- class _regFormWidgetState extends State<regWidget>
+ class _RegFormWidgetState extends State<RegWidget>
  {
   
-   TextEditingController firstnamecont =  TextEditingController();
-  TextEditingController lastnamecont = TextEditingController();
-  TextEditingController emailcont=  TextEditingController();
-  TextEditingController mobilecont =  TextEditingController();
-  TextEditingController passwordcont =  TextEditingController();
-  TextEditingController conpasswordcont=  TextEditingController();
+   TextEditingController firstnamecont = new TextEditingController();
+  TextEditingController lastnamecont = new TextEditingController();
+  TextEditingController emailcont= new TextEditingController();
+  TextEditingController mobilecont = new TextEditingController();
+  TextEditingController passwordcont = new TextEditingController();
+  TextEditingController conpasswordcont= new TextEditingController();
   
+final GlobalKey<FormState> form=GlobalKey<FormState>();
+
+      
 @override
     Widget build(BuildContext context) {
     return SafeArea(
@@ -41,131 +46,171 @@ class regWidget extends StatefulWidget
           
       body: Center(
         child: SingleChildScrollView(
-                  child: Form(
-              // key:_formKey,
-                  child: Column(mainAxisAlignment: MainAxisAlignment.center,
-      
-               children: [
-                // ignore: prefer_const_constructors
-                SizedBox(height: 20.0),
-                Container(
-                  height: 650,
-                  width: 500,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.vertical(top: Radius.elliptical(50, 60)),
-                      boxShadow: [BoxShadow(blurRadius: 10.0)]),
-                  child: Column(
-                    children: [
-                      SizedBox( height: 70.0,),
+              child: Form(
+              key:form,
+              child: Column(mainAxisAlignment: 
+              MainAxisAlignment.center,
+              children: [
+              SizedBox(height: 20.0),
+              Container(
+              height: 650,
+              width: 500,
+              decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius:
+              BorderRadius.vertical(top: Radius.elliptical(50, 60)),
+              boxShadow: [BoxShadow(blurRadius: 10.0)]),
+              child: Column( children: [
+                  SizedBox(
+                  height: 70.0,
+                    ),
                       Container(
                         height: 40.0,
                         width: 300.0,
                         child: Text(
-                          'Welcome to Register',
+                          'Welcome to Register Form',
                           style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold),
+                              color: Colors.black,
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                   
               Padding(
                 padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-                child:Container(height: 60.0,
-                width: 300.0,
+                child:Container(height: 60.0,width: 300.0,
 
-              child: TextFormField(
-              style: TextStyle(color: Colors.black),
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(labelText: "FirstName",hintStyle: TextStyle(fontSize: 14.0),
-              labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold),
-              prefixIcon: Icon(Icons.person) ),
-              controller:firstnamecont,
-            ),),),
-              Padding(
+                child: TextFormField(
+                  style: TextStyle(color: Colors.black),
+                 keyboardType: TextInputType.text,
+                  decoration: InputDecoration(labelText: "FirstName",labelStyle:TextStyle(color: Colors.black,
+                  fontSize: 17.0, fontWeight: FontWeight.bold),
+                  ),           
+                  onFieldSubmitted: (value) {},
+                    validator:( value)
+                    {
+                      if(value!.isEmpty)
+                      {
+                        return 'Please enter your first name';
+                      }
+                      return null;
+                    },
+                     ),),),
+                Padding(
                 padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-               child:Container(height: 60.0,
-                width: 300.0,
+               child:Container(height: 40.0,width: 300.0,
                 child: TextFormField(
                 style: TextStyle(color: Colors.black),
                 keyboardType: TextInputType.text,
-                decoration: InputDecoration(labelText: "LastName",hintStyle: TextStyle(fontSize: 14.0),
-                labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold),
-                prefixIcon: Icon(Icons.person) ),
+                decoration: InputDecoration(labelText: "LastName",labelStyle:TextStyle(color: Colors.black,
+                fontSize: 17.0, fontWeight: FontWeight.bold),
+                  ),    
+              onFieldSubmitted: (value) {},
                 controller:lastnamecont,
-                ),),),
-                      
+                  validator:( value)
+                  {
+                    if(value!.isEmpty)
+                    {
+                      return 'Please enter your last name';
+                    }
+                    return null;
+                  },
+                 ),),),
        
-            Padding(padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-            child: Container(
+              Padding(
+                padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
+                child:Container(height: 60.0,width: 300.0,
+                child: TextFormField(
+                style: TextStyle(color: Colors.black),
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(labelText: "Email",labelStyle:TextStyle(color: Colors.black,
+                fontSize: 17.0, fontWeight: FontWeight.bold),),  
+                  onFieldSubmitted: (value) {},controller:emailcont,
+                validator:( value)
+                {
+               if (value!.isEmpty ||
+                !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                  .hasMatch(value)) {
+                return 'Enter a valid email!';
+                  }
+                  return null;
+                },
+               ),),),
+       
+       Padding(
+                padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
+                 child:Container(height: 60.0, width: 300.0,
+                  child: TextFormField(
+                    style: TextStyle(color: Colors.black),
+                    keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(labelText: "Password",labelStyle:TextStyle(color: Colors.black,
+                  fontSize: 17.0, fontWeight: FontWeight.bold),
+                  ),  
+                        onFieldSubmitted: (value) {},obscureText: true,
+                validator:( value)
+                  {
+                    if(value!.isEmpty)
+                    {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                   ),),),
+
+        Padding(padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
+        child: Container(
                   height: 60.0,
                   width: 300.0,
                   child: TextFormField(
                     style: TextStyle(color: Colors.black),
-                     keyboardType: TextInputType.text,
-                  decoration: InputDecoration(labelText: "Email",hintStyle: TextStyle(fontSize: 14.0),
-                  labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold),
-                prefixIcon: Icon(Icons.email_outlined)),
-                controller:emailcont,
-                    ),),
-                    ),
-  Padding(padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-  child: Container(
-                  height: 60.0,
-                  width: 300.0,
-                  child: TextFormField(
-                 style: TextStyle(color: Colors.black),
-                  decoration: InputDecoration(labelText: "PhoneNumber",hintStyle: TextStyle(fontSize: 14.0),
-                  labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold),
-                  prefixIcon: Icon(Icons.phone_android_outlined)),
-                  controller: mobilecont,
-                                  ),),),
-              Padding(padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-              child: Container(
-                  height: 60.0,
-                  width: 300.0,
-                  child: TextFormField(obscureText: true,
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(labelText: "Password", hintStyle: TextStyle(fontSize: 14.0),
-              labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold,),
-                prefixIcon: Icon(Icons.lock)),
-              controller: passwordcont,
-                ),),),
-            Padding(padding:const EdgeInsets.only(bottom:15,left: 10,right: 10),
-                  child: Container(
-                  height: 60.0,
-                  width: 300.0,
-                    child: TextFormField(obscureText: true,
-                    style: TextStyle(color: Colors.black),
-                     decoration: InputDecoration(labelText: "ConfirmPassword",
-                    hintStyle: TextStyle(fontSize: 14.0),
-                    labelStyle:TextStyle(color:Colors.black,fontWeight:FontWeight.bold),
-                    prefixIcon: Icon(Icons.lock)),
-                    controller: conpasswordcont,
-                    ),),),
+                    decoration: InputDecoration(labelText: "PhoneNumber",labelStyle:TextStyle(color: Colors.black,
+                    fontSize: 17.0, fontWeight: FontWeight.bold),
+                   ),  
+                    onFieldSubmitted: (value) {},
+                    validator:( value)
+                      {
+                        if (value!.length != 10)
+                        return 'Mobile Number must be of 10 digit';
+                        else
+                        return null;
+                      }
+                        
+                       ),),),
          
                    SizedBox(
                   width: 300,
                   height: 60,
+                  // ignore: deprecated_member_use
                   child: RaisedButton(
                     color: Colors.redAccent,
-                    onPressed: (){},
-                    
-                 shape: RoundedRectangleBorder(
+                    onPressed: (){
+                        if(form.currentState!.validate())
+                      {
+                        print("successful");
+                        return;
+                      }else{
+                        print("UnSuccessfull");
+                      }
+                    },
+                    shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(50.0),
                         side: BorderSide(color: Colors.blue,width: 2)
-                    ),textColor:Colors.white,child: Text("Submit") 
-                
-                  ),),
-  
-            ],
-          ),
-        ),],
-      ),
-    ),),),),);
-  }
-}
+                    ),
+                     textColor:Colors.white,child: Text("Submit"),
+                        ),
+                        ),
+                        ],
+                       ),
+                       ),
+                       ],
+                       ),
+                       ),
+                       ),
+                       ),
+                       ),
+                       );
+                        }
+                        }
 
  
 
